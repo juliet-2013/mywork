1cd686a8236b21754e873521c301647ee913ae7b
Juliet Thuku <juliet.thuku@student.moringaschool.com>
2023-06-17 12:25:15 +0300

    capitalize on python

---
+++ work.py
@@ -0,0 +1,8 @@
+def capitalize_string(string):
+    capitalized_string = string.capitalize()
+    return capitalized_string
+
+# Example usage
+input_string = input("Enter a string: ")
+result = capitalize_string(input_string)
+print("Capitalized string:", result)
